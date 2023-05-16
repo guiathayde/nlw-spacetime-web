8af575ed305f76220b51c1e02b0430c0bb74f4a2
Guilherme Athayde <guiathayde303@gmail.com>
2023-05-16 12:40:55 -0300

    Initial setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
-import './globals.css'
+import { ReactNode } from 'react'
 import { Inter } from 'next/font/google'
+
+import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,7 +13,7 @@ export const metadata = {
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: ReactNode
 }) {
   return (
     <html lang="en">
